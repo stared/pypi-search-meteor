1b0a8c9543b91aa0a7f4e6b601c6f071f1471195
Piotr Migdal <pmigdal@gmail.com>
2014-04-19 17:59:45 +0200

    now it grabs data, finally

--- client/main.js
+++ client/main.js
@@ -3,14 +3,28 @@ Meteor.subscribe('packages');
 Template.packageList.helpers({
   packages: function() {
     // return [{api_name: "aasd", description: "desc desc"}];
-    return Pypis.find({}, {sort: {api_name: -1}, limit: 20});
+    var re = new RegExp("req", "i");
+    return Pypis.find({api_name: re}, {sort: {api_name: 1}, limit: 10});
   }
 });
 
 
 Template.packageItem.helpers({
-  url: function(x) {
-    return "https://pypi.python.org/pypi/" + x.api_name; 
+  url: function() {
+    return "https://pypi.python.org/pypi/" + this.api_name; 
     // + "/" + x.api_version;
+  },
+  more: function() {
+    if (this.hasOwnProperty('info')) {
+      return this.info.downloads.last_month;
+    } else {
+      console.log('calling meteor!');
+      Meteor.call('updatePackageEntry',
+                  this.api_name,
+                  this._id,
+                  function (error, data) {
+        return "Done!";
+      });
+    }
   }
 });
--- server/startup.js
+++ server/startup.js
@@ -10,7 +10,9 @@
 // }
 
 Meteor.publish('packages', function() {
-  return Pypis.find({}, {sort: {api_name: -1}, limit: 20}); 
+  // return Pypis.find({}, {sort: {api_name: 1}, limit: 20});
+  var re = new RegExp("req", "i");
+  return Pypis.find({api_name: re}, {sort: {api_name: 1}, limit: 10})
 });
 
 if (Pypis.find().count() === 0) {
@@ -18,7 +20,20 @@ if (Pypis.find().count() === 0) {
   var site2 = "https://pypi.python.org/pypi/requests/json";
   var z = Meteor.http.get(site2).data;
   Pypis.insert(z);
-};
+}
+
+Meteor.methods({
+  updatePackageEntry: function (name, id) {
+    var jsonUrl = "https://pypi.python.org/pypi/" + name + "/json";
+    var res = Meteor.http.get(jsonUrl).data;
+    delete res.releases;
+    // ^ as it is a tricky field with dots in keys
+    // but I will need to process it one way or the other
+    Pypis.update({_id: id}, {$set: res});
+    return "Done!";
+  }
+});
+
   // var res = httpGetJSON("https://pypi.python.org/pypi/requests/json");
 //   Pypis.insert(res);
 
