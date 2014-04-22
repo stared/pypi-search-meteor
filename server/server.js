fd1f3c97c1113cd2e9bd6d4f384f198eb6e327a4
Piotr Migdal <pmigdal@gmail.com>
2014-04-22 14:18:35 +0200

    npm an cheerio init

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+var cheerio = Meteor.require('cheerio');
+
 Meteor.publish('packages', function(name_query) {
   // return Pypis.find({}, {sort: {api_name: 1}, limit: 20});
   var re = new RegExp(name_query, "i");
@@ -15,5 +17,15 @@ Meteor.methods({
     // but I will need to process it one way or the other
     Pypis.update({_id: id}, {$set: res});
     return "Done!";
+  },
+  testAutoparsing: function () {
+  var data = Meteor.http.get("https://pypi.python.org/pypi").content;
+  var $ = cheerio.load(data);
+  // var res = $('table.list a');
+  var packages = [];
+  $('table.list a').each(function(i, elem) {
+    packages[i] = $(this).attr('href');
+  });
+  return packages;
   }
 });
