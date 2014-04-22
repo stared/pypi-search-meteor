f29dda03ea5894ded71c84cdd0bb07dd13e8517a
Piotr Migdal <pmigdal@gmail.com>
2014-04-22 22:13:30 +0200

    autoloads package list when db is empty

--- server/server.js
+++ server/server.js
@@ -8,6 +8,29 @@ Meteor.publish('packages', function(name_query) {
 
 // var sanitizeReleases = function ...
 
+Meteor.startup(function () {
+  if (Pypis.find().count() === 0) {
+    var address = "https://pypi.python.org/pypi?%3Aaction=index";
+    var $ = cheerio.load(Meteor.http.get(address).content);
+    var packages = [];
+    var packagesDict = {};
+    $('table.list a').each(function(i, elem) {
+      packages[i] = $(this).attr('href');
+    });
+    packages = packages.map(function (x) {
+      var split = x.split("/");
+      return split[split.length - 2];
+    });
+    for (var i = 0; i < packages.length; i++) {
+      if (!(packages[i] in packagesDict)) {
+        Pypis.insert({api_name: packages[i],
+                      description: "Will load later."});
+        packagesDict[packages[i]] = true;
+      }
+    }
+  }
+});
+
 Meteor.methods({
   updatePackageEntry: function (name, id) {
     var jsonUrl = "https://pypi.python.org/pypi/" + name + "/json";
@@ -18,8 +41,8 @@ Meteor.methods({
     Pypis.update({_id: id}, {$set: res});
     return "Done!";
   },
-  testAutoparsing: function (address) {
-  var address = address || "https://pypi.python.org/pypi";
+  testAutoparsing: function (path) {
+  var address = path || "https://pypi.python.org/pypi";
   var data = Meteor.http.get(address).content;
   var $ = cheerio.load(data);
   // var res = $('table.list a');
